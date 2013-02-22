cd5589f4d400a997fbbf93d8df6831506c481fc6
Duncan Angus Wilkie (mrdnk) <duncanwilkie@hotmail.com>
2013-02-22 14:33:57 +0000

    Updated bark route

--- routes/bark.js
+++ routes/bark.js
@@ -1,30 +1,48 @@
 /* barker - the API main entrance for logging */
+var fs = require('fs');
 
-function Bark () {
-  this.app = null;
-  this.page = null;
-  this.msg = null;
-  this.stackTrace = null;
-  this.userID = null;
-  this.authenticationSystem = null;
-  this.environment = null; // production, development, testing, staging
-  this.server = null;
-  this.customer = null;
+function Bark (data) {
+
+  var b = this.b = {};
+
+  b.app = data.app || null;
+  b.page = data.page || null;
+  b.msg = data.msg || null;
+  b.stackTrace = data.stackTrace || null;
+  b.userID = data.userID || null;
+  b.authenticationSystem = data.authenticationSystem || null;
+  b.environment = data.environment || null; // production, development, testing, staging
+  b.server = data.server || null;
+  b.customer = data.customer || null;
+
+  this.b = b;
+
+  return this;
 }
 
-Bark.prototype.saveToDB () {
+Bark.prototype.saveToDB = function () {
   var that = this;
 
-  
-  
+  //database stuff
+
+}
+
+Bark.prototype.saveToFile = function () {
+  var that = this;
+
+  fs.writeFile('barker.json', JSON.stringify(that.b, null, 2), function (err) {
+  if (err) throw err;
+    console.log('It\'s saved!');
+  });
 }
 
 exports.index = function (req, res) {
   
   var params = req.body;
 
-  var bark = new Bark();
-  bark.app          = params.app || null;;
+  var bark = new Bark(params);
+
+/*  bark.app          = params.app || null;;
   bark.page         = params.page || null;;
   bark.msg          = params.msg || null;;
   bark.stackTrace   = params.stackTrace || null;;
@@ -32,10 +50,11 @@ exports.index = function (req, res) {
   bark.authenticationSystem = params.authenticationSystem || null;;
   bark.environment  = params.environment || 'unknown';
   bark.server       = params.server || null;
-  bark.customer     = params.customer || null;
+  bark.customer     = params.customer || null;*/
 
-  console.log(bark);
+  console.log (bark);
 
+  bark.saveToFile ();
   res.json('done')
 
 }
